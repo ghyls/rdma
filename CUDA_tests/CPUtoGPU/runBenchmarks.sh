411712342d2308f2ed3063429c59261795fbedf6
Mario Gonzalez Carpintero <mario.gonzalez.carpintero@cern.ch>
2019-07-15 17:23:07 +0200

    it works with UCX and w/o it

--- CUDA_tests/CPUtoGPU/runBenchmarks.sh
+++ CUDA_tests/CPUtoGPU/runBenchmarks.sh
@@ -1,7 +1,23 @@
 
 
+echo "WITHOUT UCX -------------------------------------------------------------"
 
+echo "(one step) host to host"
+mpirun -np 2 --hostfile hosts --mca pml ^ucx --mca btl ^openib main 1 1 0 > ./benchmarksResults/noUCX_110.dat
 
-# WITHOUT UCX
+echo "one step host to device"
+mpirun -np 2 --hostfile hosts --mca pml ^ucx --mca btl ^openib main 1 0 1 > ./benchmarksResults/noUCX_101.dat
 
-mpirun -np 2 --hostfile hosts --mca pml ucx --mca btl ^openib main
+echo "two steps host to device"
+mpirun -np 2 --hostfile hosts --mca pml ^ucx --mca btl ^openib main 0 0 1 > ./benchmarksResults/noUCX_010.dat
+
+echo "WITH UCX ----------------------------------------------------------------"
+
+echo "(one step) host to host"
+mpirun -np 2 --hostfile hosts --mca pml ucx --mca btl ^openib main 1 1 0 > ./benchmarksResults/UCX_110.dat
+
+echo "one step host to device"
+mpirun -np 2 --hostfile hosts --mca pml ucx --mca btl ^openib main 1 0 1 > ./benchmarksResults/UCX_101.dat
+
+echo "two steps host to device"
+mpirun -np 2 --hostfile hosts --mca pml ucx --mca btl ^openib main 0 0 1 > ./benchmarksResults/UCX_010.dat
