df452534fd522208d191e6d540cf281bef8a6ffb
Mario <marioyales2@gmail.com>
2019-07-22 16:20:57 +0200

    added Ethernet measurement

--- CUDA_tests/CPUtoGPU/runBenchmarks.sh
+++ CUDA_tests/CPUtoGPU/runBenchmarks.sh
@@ -1,4 +1,13 @@
 
+nvcc -I/home/mariog/openmpi-4.0.1/build/include -L/home/mariog/openmpi-4.0.1/build/lib -lmpi bufferSendingBenchmarks.cu -o main
+scp -r *  mariog@felk40.cern.ch:/home/mariog/CUDA_tests/CPUtoGPU
+
+
+echo "WITH ETH ----------------------------------------------------------------"
+
+echo "(one step) host to host"
+mpirun -np 2 --hostfile hosts --mca pml ob1 --mca btl ^openib main 1 1 0 > ./benchmarksResults/ETH_110.dat
+
 
 echo "WITHOUT UCX -------------------------------------------------------------"
 
@@ -6,7 +15,7 @@ echo "(one step) host to host"
 mpirun -np 2 --hostfile hosts --mca pml ^ucx --mca btl ^openib main 1 1 0 > ./benchmarksResults/noUCX_110.dat
 
 echo "two steps host to device"
-mpirun -np 2 --hostfile hosts --mca pml ^ucx --mca btl ^openib main 0 0 1 > ./benchmarksResults/noUCX_010.dat
+mpirun -np 2 --hostfile hosts --mca pml ^ucx --mca btl ^openib main 0 0 1 > ./benchmarksResults/noUCX_001.dat
 
 echo "WITH UCX ----------------------------------------------------------------"
 
@@ -17,4 +26,6 @@ echo "one step host to device"
 mpirun -np 2 --hostfile hosts --mca pml ucx --mca btl ^openib main 1 0 1 > ./benchmarksResults/UCX_101.dat
 
 echo "two steps host to device"
-mpirun -np 2 --hostfile hosts --mca pml ucx --mca btl ^openib main 0 0 1 > ./benchmarksResults/UCX_010.dat
+mpirun -np 2 --hostfile hosts --mca pml ucx --mca btl ^openib main 0 0 1 > ./benchmarksResults/UCX_001.dat
+
+echo "PATATRACK cudaMemcpy() --------------------------------------------------"
