42dec9e67777dcab72d54eda997106617bb6f75d
Mario <marioyales2@gmail.com>
2019-07-30 18:43:51 +0200

    used for another purpose. The previous code has ben commented.

--- CUDA_tests/CPUtoGPU/benchmarksResults/doPlots.py
+++ CUDA_tests/CPUtoGPU/benchmarksResults/doPlots.py
@@ -5,30 +5,37 @@ from plotter import Plotter
 
 p = Plotter((12,7))
 
-ETH_110 =   np.array([k[1] for k in np.loadtxt("ETH_110.dat")][1:])
-noUCX_001 = np.array([k[1] for k in np.loadtxt("noUCX_001.dat")][1:])
-noUCX_110 = np.array([k[1] for k in np.loadtxt("noUCX_110.dat")][1:])
-UCX_001 =   np.array([k[1] for k in np.loadtxt("UCX_001.dat")][1:])
-UCX_101 =   np.array([k[1] for k in np.loadtxt("UCX_101.dat")][1:])
-UCX_110 =   np.array([k[1] for k in np.loadtxt("UCX_110.dat")][1:])
-Memcpy =    np.array([k[1] for k in np.loadtxt("Memcpy.dat")][1:])
+#ETH_110 = [k[1] for k in np.loadtxt("ETH_110.dat")][1:]
+#noUCX_001 = [k[1] for k in np.loadtxt("noUCX_001.dat")][1:]
+#noUCX_110 = [k[1] for k in np.loadtxt("noUCX_110.dat")][1:]
+UCX_001_Y =   [k[1] for k in np.loadtxt("UCX_001_Y.dat")][1:]
+UCX_101_Y =   [k[1] for k in np.loadtxt("UCX_101_Y.dat")][1:]
+UCX_110_Y =   [k[1] for k in np.loadtxt("UCX_110_Y.dat")][1:]
+UCX_001_N =   [k[1] for k in np.loadtxt("UCX_001_N.dat")][1:]
+UCX_101_N =   [k[1] for k in np.loadtxt("UCX_101_N.dat")][1:]
+UCX_110_N =   [k[1] for k in np.loadtxt("UCX_110_N.dat")][1:]
+#Memcpy =   [k[1] for k in np.loadtxt("Memcpy.dat")][1:]
 
-pSize = [k[0] for k in np.loadtxt("noUCX_001.dat")][1:]
+pSize = [k[0] for k in np.loadtxt("UCX_001_Y.dat")][1:]
 
 p.createAxis(111)
 
+#p.addSubplot(pSize, ETH_110, dataLabel=r"ETH: H$\rightarrow$H", dataStyle=".-", width=2, size=8)
+#p.addSubplot(pSize, noUCX_001, dataLabel=r"noUCX: H$\rightarrow$H$\rightarrow$D", dataStyle=".-", width=2, size=8)
+#p.addSubplot(pSize, noUCX_110, dataLabel=r"noUCX: H$\rightarrow$H", dataStyle=".-", width=2, size=8)
+p.addSubplot(pSize, UCX_001_Y, dataLabel=r"UCX_Y: H$\rightarrow$H$\rightarrow$D", dataStyle=".-", width=2, size=8)
+p.addSubplot(pSize, UCX_101_Y, dataLabel=r"UCX_Y: H$\rightarrow$D", dataStyle=".-", width=2, size=8)
+p.addSubplot(pSize, UCX_110_Y, dataLabel=r"UCX_Y: H$\rightarrow$H", dataStyle=".-", width=2, size=8)
+p.addSubplot(pSize, UCX_001_N, dataLabel=r"UCX_N: H$\rightarrow$H$\rightarrow$D", dataStyle=".-", width=2, size=8)
+p.addSubplot(pSize, UCX_101_N, dataLabel=r"UCX_N: H$\rightarrow$D", dataStyle=".-", width=2, size=8)
+p.addSubplot(pSize, UCX_110_N, dataLabel=r"UCX_N: H$\rightarrow$H", dataStyle=".-", width=2, size=8)
 
-mult = 1000
+#p.addSubplot(pSize, Memcpy, dataLabel=r"cudaMemcpy", dataStyle=".-", width=2, size=8)
 
-p.addSubplot(pSize, ETH_110*mult, dataLabel=r"ETH: H$\rightarrow$H", dataStyle=".-", width=2, size=8)
-p.addSubplot(pSize, noUCX_001*mult, dataLabel=r"noUCX: H$\rightarrow$H$\rightarrow$D", dataStyle=".-", width=2, size=8)
-p.addSubplot(pSize, noUCX_110*mult, dataLabel=r"noUCX: H$\rightarrow$H", dataStyle=".-", width=2, size=8)
-p.addSubplot(pSize, UCX_001*mult, dataLabel=r"UCX: H$\rightarrow$H$\rightarrow$D", dataStyle=".-", width=2, size=8)
-p.addSubplot(pSize, UCX_101*mult, dataLabel=r"UCX: H$\rightarrow$D", dataStyle=".-", width=2, size=8)
-p.addSubplot(pSize, UCX_110*mult, dataLabel=r"UCX: H$\rightarrow$H", dataStyle=".-", width=2, size=8)
-#p.addSubplot(pSize, Memcpy*mult, dataLabel=r"cudaMemcpy", dataStyle=".-", width=2, size=8)
 
-p.setTitles("OneStep HtoH HtoDev", "size (MB)", "time (ms)")
+
+
+p.setTitles("OneStep HtoH HtoDev", "size (MB)", "time (s)")
 p.setProperties(grid=True, doXlog=1, doYlog=1)
 
 p.saveFig("throughput.png", dpi=200)
