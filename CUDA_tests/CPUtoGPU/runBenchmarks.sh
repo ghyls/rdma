26d0afabb08bf066f42d4aacaee2008020027743
Mario <marioyales2@gmail.com>
2019-07-30 18:44:55 +0200

    changed a bit, so it can produce the new benchmarksResults/throughput.png

--- CUDA_tests/CPUtoGPU/runBenchmarks.sh
+++ CUDA_tests/CPUtoGPU/runBenchmarks.sh
@@ -3,29 +3,37 @@
 scp -r *  mariog@felk40.cern.ch:/home/mariog/CUDA_tests/CPUtoGPU
 
 
-echo "WITH ETH ----------------------------------------------------------------"
+#echo "WITH ETH ----------------------------------------------------------------"
+#
+#echo "(one step) host to host"
+#mpirun -np 2 --hostfile hosts --mca pml ob1 --mca btl ^openib main 1 1 0 0 > ./benchmarksResults/ETH_110.dat
+#
+#echo "WITHOUT UCX -------------------------------------------------------------"
+#
+#echo "(one step) host to host"
+#mpirun -np 2 --hostfile hosts --mca pml ^ucx --mca btl ^openib main 1 1 0 0 > ./benchmarksResults/noUCX_110.dat
+#
+#echo "two steps host to device"
+#mpirun -np 2 --hostfile hosts --mca pml ^ucx --mca btl ^openib main 0 0 1 0 > ./benchmarksResults/noUCX_001.dat
+#
+echo "WITH UCX, UCX_MEMTYPE_CACHE=y -------------------------------------------"
 
 echo "(one step) host to host"
-mpirun -np 2 --hostfile hosts --mca pml ob1 --mca btl ^openib main 1 1 0 0 > ./benchmarksResults/ETH_110.dat
-
-echo "WITHOUT UCX -------------------------------------------------------------"
-
-echo "(one step) host to host"
-mpirun -np 2 --hostfile hosts --mca pml ^ucx --mca btl ^openib main 1 1 0 0 > ./benchmarksResults/noUCX_110.dat
-
-echo "two steps host to device"
-mpirun -np 2 --hostfile hosts --mca pml ^ucx --mca btl ^openib main 0 0 1 0 > ./benchmarksResults/noUCX_001.dat
-
-echo "WITH UCX ----------------------------------------------------------------"
-
-echo "(one step) host to host"
-mpirun -np 2 --hostfile hosts --mca pml ucx --mca btl ^openib main 1 1 0 0 > ./benchmarksResults/UCX_110.dat
+mpirun -np 2 --hostfile hosts --mca pml ucx --mca btl ^openib main 1 1 0 0 > ./benchmarksResults/UCX_110_Y.dat
 
 echo "one step host to device"
-mpirun -np 2 --hostfile hosts --mca pml ucx --mca btl ^openib main 1 0 1 0 > ./benchmarksResults/UCX_101.dat
+mpirun -np 2 --hostfile hosts --mca pml ucx --mca btl ^openib main 1 0 1 0 > ./benchmarksResults/UCX_101_Y.dat
 
 echo "two steps host to device"
-mpirun -np 2 --hostfile hosts --mca pml ucx --mca btl ^openib main 0 0 1 0 > ./benchmarksResults/UCX_001.dat
+mpirun -np 2 --hostfile hosts --mca pml ucx --mca btl ^openib main 0 0 1 0 > ./benchmarksResults/UCX_001_Y.dat
 
-echo "PATATRACK cudaMemcpy() --------------------------------------------------"
-mpirun -np 2 --hostfile hosts --mca pml ucx --mca btl ^openib main 0 0 0 1 > ./benchmarksResults/Memcpy.dat
+echo "WITH UCX, UCX_MEMTYPE_CACHE=n -------------------------------------------"
+
+echo "(one step) host to host"
+mpirun -np 2 --hostfile hosts -x UCX_MEMTYPE_CACHE=n --mca pml ucx --mca btl ^openib main 1 1 0 0 > ./benchmarksResults/UCX_110_N.dat
+
+echo "one step host to device"
+mpirun -np 2 --hostfile hosts -x UCX_MEMTYPE_CACHE=n --mca pml ucx --mca btl ^openib main 1 0 1 0 > ./benchmarksResults/UCX_101_N.dat
+
+echo "two steps host to device"
+mpirun -np 2 --hostfile hosts -x UCX_MEMTYPE_CACHE=n --mca pml ucx --mca btl ^openib main 0 0 1 0 > ./benchmarksResults/UCX_001_N.dat
