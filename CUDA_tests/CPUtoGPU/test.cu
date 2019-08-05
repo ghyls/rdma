8bc0146b8c902f934f93c1e87a96186cc5d30f77
Mario <marioyales2@gmail.com>
2019-08-05 09:13:39 +0200

    modified for debugging reasons.

--- CUDA_tests/CPUtoGPU/test.cu
+++ CUDA_tests/CPUtoGPU/test.cu
@@ -1,10 +1,10 @@
 
-
-# include <iostream>
+#include <cstring>
+#include <iostream>
 #include <stdio.h>
-# include <cuda.h>
-# include <cuda_runtime.h>
-# include "mpi.h"
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include "mpi.h"
 
 
 #define CHECK_ERROR( err, msg ) if( err != cudaSuccess ) { std::cerr <<"\033[1;31mERROR:" << cudaGetErrorName ( err ) << "  |  " << "ERROR DES: " <<cudaGetErrorString( err ) << "  |  " << "User msg: " << msg << "\033[0m" <<std::endl; exit( 0 ); }
@@ -12,9 +12,9 @@
 
 
 // number of elements of the host buffer. All of them are sent to the device. 
-#define N 16
+#define N 41000
 // number of elements that will be received on the dev buffer. M must be <= N.
-#define M 16
+#define M N
 
 // When N = M ~ 1e5             -> Everything works.
 // When N = M < 4.2e4 (0.84 MB) -> Segmentation fault (non UCX related)
@@ -30,9 +30,10 @@ be most likely because MPI doesn't start writing the buffer right at the
 beginning. I am still trying to prove this.*/
 
 
-int main()
+int main(int argc, const char * argv[])
 {
     int rank;
+    int size = argc > 1 ? atoi(argv[1]) : N;
     
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -41,39 +42,42 @@ int main()
     float *buf_dev  = nullptr;
 
     // the size of both buffers (not necessarily the size of the package)
-    //const long bufferSize = N*sizeof(float);
-    int bufferSize = N*sizeof(float);
+    //const long bufferSize = size*sizeof(float);
+    int bufferSize = size*sizeof(float);
 
-    // Host buffer: The four following alternatives give the same result. I am
-    // pretty sure that the error has nothing to do with the host.
-    // -----
-    //cudaMallocHost((void **) &buf_host, bufferSize);
-    //cudaMallocHost((void **) &buf_host, bufferSize);
-    buf_host = (float*)malloc(bufferSize);
-    //buf_host = new float[N];
-    //float buf_host[N];
-    // -----
-
-    if (rank==1) printf("buf_dev before cudaMalloc on rank %d: %p\n", rank, buf_dev);
+    // Host buffer
+    if (rank==0) {
+        buf_host = (float*)malloc(bufferSize);
+        std::cout << "size (MB): " << bufferSize / 1048576. << std::endl;
+    }
 
     // Device buffer
-    //cudaMalloc((void **) &buf_dev, bufferSize);   // does not work
-    //CHECK_ERROR( cudaMalloc((void **)buf_dev, bufferSize), "hello");    // works with small pkgs
-    //cudaMalloc((void **)buf_dev, bufferSize);    // works with small pkgs
-    cudaMalloc(&buf_dev, bufferSize);               // works with small pkgs
-
-    if (rank==1) printf("buf_dev after cudaMalloc on rank %d: %p\n", rank, buf_dev);
-
-    if(rank == 0) {
-        MPI_Ssend(buf_host, N, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
-        //printf("buf_dev after MPI_Ssend on rank %d: %p\n", rank, buf_dev);
-    
+    if (rank==1) {
+        printf("buf_dev before cudaMalloc on rank %d: %p\n", rank, buf_dev);
+	cudaMalloc(&buf_dev, bufferSize);               // works with small pkgs
+	//cudaMalloc(&buf_dev, 16*1024*1024);               // works with small pkgs
+        printf("buf_dev after cudaMalloc on rank %d: %p\n", rank, buf_dev);
     }
-    else{ 
+
+    if (rank == 0) {
+        MPI_Send(buf_host, size, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
+        //printf("buf_dev after MPI_Ssend on rank %d: %p\n", rank, buf_dev);
+    } else { 
         // receive into the device buffer
         printf("buf_dev before MPI_recv on rank %d: %p\n", rank, buf_dev);
         MPI_Recv(buf_dev, M, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
+
+    // Host buffer
+    if (rank==0) {
+        free(buf_host);
+    }
+
+    // Device buffer
+    if (rank==1) {
+	cudaFree(buf_dev);
+    }
+
     MPI_Finalize();
 }
 
