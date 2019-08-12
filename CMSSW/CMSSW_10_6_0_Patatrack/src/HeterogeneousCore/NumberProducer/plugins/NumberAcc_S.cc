fc9902ec3a364f933d664d9ce490a1bea572afd7
Mario <marioyales2@gmail.com>
2019-08-12 19:11:42 +0200

    the accumulator, only program that will run on the server

---
+++ CMSSW/CMSSW_10_6_0_Patatrack/src/HeterogeneousCore/NumberProducer/plugins/NumberAcc_S.cc
@@ -0,0 +1,87 @@
+// system include files
+#include <memory>
+#include <iostream>
+//#include <mpi.h>
+
+// user include files
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/Utilities/interface/StreamID.h"
+
+
+// class declaration
+
+class NumberAcc_S : public edm::stream::EDProducer<> {
+public:
+    explicit NumberAcc_S(const edm::ParameterSet& config);
+    ~NumberAcc_S() = default;
+
+    static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+private:
+    virtual void beginStream(edm::StreamID) override;
+    virtual void produce(edm::Event& event, const edm::EventSetup& setup) override;
+    virtual void endStream() override;
+
+    const edm::EDGetTokenT<std::vector<double>> data_;
+};
+
+NumberAcc_S::NumberAcc_S(const edm::ParameterSet& config) :
+    data_(consumes<std::vector<double>>(config.getParameter<edm::InputTag>("data")))
+{
+   
+   produces<std::vector<double>>(); // only consisting on one element!
+
+}
+
+
+void
+NumberAcc_S::produce(edm::Event& event, const edm::EventSetup& setup)
+{
+
+
+
+    // read from the NumberProducer
+    edm::Handle<std::vector<double>> handle;
+    event.getByToken(data_, handle);
+    auto const& data = * handle;
+
+    // sum all the elements in data
+    double sum = 0;
+    for (double item: data)
+        sum += item;
+
+    // Create the output vector
+    auto result = std::make_unique<std::vector<double>>(1); // only one element!
+
+    // fill it with sum
+    (*result)[0] = (double) sum;
+    
+    event.put(std::move(result));
+}
+
+void
+NumberAcc_S::beginStream(edm::StreamID)
+{
+}
+
+void
+NumberAcc_S::endStream() {
+}
+
+void
+NumberAcc_S::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  // NumberAcc_S
+  edm::ParameterSetDescription desc;
+  desc.add<edm::InputTag>("data", { "numberProducer" });
+  descriptions.add("numberAcc_S", desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(NumberAcc_S);
+
