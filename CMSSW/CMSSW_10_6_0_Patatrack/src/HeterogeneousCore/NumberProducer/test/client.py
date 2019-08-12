dbf6c07674a64a84b0a1f53556490b0b4c87df9e
Mario <marioyales2@gmail.com>
2019-08-12 19:11:16 +0200

    this is the client python configuration file (rank 0)

---
+++ CMSSW/CMSSW_10_6_0_Patatrack/src/HeterogeneousCore/NumberProducer/test/client.py
@@ -0,0 +1,55 @@
+import FWCore.ParameterSet.Config as cms
+
+
+process = cms.Process("TEST")
+#process.MPIService  = cms.Service("MPIService")
+
+#process.Tracer = cms.Service("Tracer")
+
+
+# All the objects in the chain 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# The data is produced from scratch on the producer.
+process.source = cms.Source("EmptySource")
+
+# Edit variables called from NumberProducer::NumberProducer
+process.load('HeterogeneousCore.NumberProducer.numberProducer_cfi')
+process.numberProducer.size = 10
+process.numberProducer.seed = 65
+
+# There is no need to do anything on the Accumulator
+process.load('HeterogeneousCore.NumberProducer.numberAccumulator_cfi')
+process.numberAccumulator.data = 'numberProducer'
+
+# Edit variables called from NumberLogger::NumberLogger
+process.load('HeterogeneousCore.NumberProducer.numberLogger_cfi')
+process.numberLogger.data = 'numberAccumulator'
+
+# We can create another logger
+process.otherLogger = process.numberLogger.clone()
+process.otherLogger.data = 'numberProducer'
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# Tasks and paths 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# create the first task
+process.task = cms.Task( process.numberProducer )
+
+# Append elements to the task. 
+# Note that process.* do not define a commutative group under "+"!
+process.path1 = cms.Path(  process.numberAccumulator + process.numberLogger, process.task )
+#process.path2 = cms.Path( process.otherLogger, process.task )
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# Extra options
+process.options = cms.untracked.PSet(
+    numberOfStreams = cms.untracked.uint32(0),
+    numberOfThreads = cms.untracked.uint32(1),
+    wantSummary = cms.untracked.bool(True)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(20)
+)
