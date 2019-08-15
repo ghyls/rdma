0cfadd4ec0c50dd93d5b065f0635a82b886225b7
Mario <marioyales2@gmail.com>
2019-08-15 10:03:35 +0200

    implemented the CUDA and MPI API

--- CMSSW/CMSSW_10_6_0_Patatrack/src/HeterogeneousCore/NumberProducer/plugins/NumberAccS.cc
+++ CMSSW/CMSSW_10_6_0_Patatrack/src/HeterogeneousCore/NumberProducer/plugins/NumberAccS.cc
@@ -2,6 +2,9 @@
 #include <memory>
 #include <iostream>
 #include <mpi.h>
+#include <cuda.h>
+#include "HeterogeneousCore/CUDAUtilities/interface/cudaCheck.h"
+#include "header.h"
 
 // user include files
 #include "FWCore/Framework/interface/Event.h"
@@ -34,17 +37,17 @@ private:
 };
 
 
+
 NumberAccS::NumberAccS(const edm::ParameterSet& config)
 {
-    LOG("[NumberAccS::NumberAccS]:  initializing MPI enviroment...", 0);
-    MPI_Init(NULL, NULL);
+    LOG("[NumberAccS::NumberAccS]:  Constructor called. initialized MPI enviroment", 2);
+    //MPI_Init(NULL, NULL);
     produces<std::vector<double>>(); // only consisting on one element!
-    LOG("[NumberAccS::NumberAccS]:  done", 0);
 }
 NumberAccS::~NumberAccS()
 {
-    LOG("[NumberAccS::~NumberAccS]:  destruting...", 0);
-    MPI_Finalize();
+    //MPI_Finalize();
+    LOG("[NumberAccS::~NumberAccS]:  Destructor called. Finalized MPI enviroment", 2);
 }
 
 void
@@ -58,30 +61,93 @@ NumberAccS::produce(edm::Event& event, const edm::EventSetup& setup)
     LOG("[NumberAccS::produce]:  RANK: " + std::to_string(rank), 1);
     LOG("[NumberAccS::produce]:  SIZE: " + std::to_string(size), 1);
     // Only rank 1 is supposed to be here
-    if (rank == 1)
+
+    int len;
+
+    // Recv the len of the buffer
+    MPI_Recv(&len, 1, MPI_INT, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    LOG("[NumberAccS::produce]:  len Received", 0);
+
+    // Allocate the memory
+    //auto input = std::make_unique<double[]>(len);
+
+    bool useGPU    = true;
+    bool GPUDirect = true;
+
+    double *input;
+    if (!GPUDirect)
     {
-        int len;
-
-        // Recv the len of the buffer
-        MPI_Recv(&len, 1, MPI_INT, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        LOG("[NumberAccS::produce]:  len Received", 0);
-
-        // Allocate the memory
-        auto input = std::make_unique<double[]>(len);
-
+        input = (double*)malloc(len * sizeof(double));   // host buffer
         // Recv the actual buffer
         MPI_Recv(&input[0], len, MPI_DOUBLE, 0, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        LOG("[NumberAccS::produce]:  data Received", 0);
-
-        // sum all the input elements 
-        double sum = 0;
-        for (int i = 0; i < len; i++)
-            sum += input[i];
-
-        // send the sum back to the offloader
-        MPI_Ssend(&sum, 1, MPI_DOUBLE, 0, 101, MPI_COMM_WORLD);
-        LOG("[NumberAccS::produce]:  result Sent", 0);
+        LOG("[NumberAccS::produce]:  data Received", 0);   
     }
+
+    // this is the variable on the host where the result will be stored
+    double *sum_h = (double*)malloc(sizeof(double));   // host buffer
+    if (useGPU)
+    {
+        // do the computation in a CUDA kernel
+        LOG("[NumberAccS::produce]:  Using the GPU to compute the sum", 0);
+
+        // The array to be summed will be stored here on the GPU
+        double *buf_d;
+        cudaCheck( cudaMalloc((void **) &buf_d, len * sizeof(double)) );
+
+        // and this will be its sum also on the GPU
+        double *sum_d;
+        cudaCheck( cudaMalloc((void **) &sum_d, sizeof(double)) );
+
+        if (GPUDirect){
+            LOG("[NumberAccS::produce]:  Receiving the package on the GPU", 0);
+            MPI_Recv(buf_d, len, MPI_DOUBLE, 0, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            LOG("[NumberAccS::produce]:  Received the package on the GPU", 0);
+        }
+        else{
+            // Move the host array to the GPU
+            cudaMemcpy(buf_d, input, len * sizeof(double), cudaMemcpyHostToDevice);            
+        }
+
+        // Pass sum_h to the wrapper, so it can change its value to the actual
+        // sum (which is what we want)
+        cudaWrapper(buf_d, sum_d, len);
+
+        if (GPUDirect)
+        {
+            LOG("[NumberAccS::produce]:  Sending the result (DtoH)", 0);
+            LOG(std::to_string(rank), 2);
+            MPI_Ssend(sum_d, 1, MPI_DOUBLE, 0, 101, MPI_COMM_WORLD);
+            LOG("[NumberAccS::produce]:  result Sent (DtoH)", 0);
+        }
+        else
+        {
+            cudaCheck( cudaMemcpy(sum_h, sum_d, sizeof(double), cudaMemcpyDeviceToHost) );    
+            LOG("[NumberAccS::produce]:   Result: " + std::to_string(sum_h[0]), 0);
+            
+            // send the sum back to the offloader
+            MPI_Ssend(sum_h, 1, MPI_DOUBLE, 0, 101, MPI_COMM_WORLD);
+            LOG("[NumberAccS::produce]:  result Sent (HtoH)", 0);
+        }
+        
+
+        cudaFree(buf_d);
+        cudaFree(sum_d);
+        // get back the result from the GPU
+
+
+    }
+    else
+    {
+        LOG("[NumberAccS::produce]:  Not using any kernel", 0);
+        // sum all the input elements 
+        sum_h[0] = 0;
+        for (int i = 0; i < len; i++)
+            sum_h[0] += input[i];
+    }
+
+
+
+    
 }
 
 void
