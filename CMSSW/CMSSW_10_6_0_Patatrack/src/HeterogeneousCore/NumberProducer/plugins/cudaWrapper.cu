f0c4d692171f6694af88086222b0ab9e1a1eb723
Mario <marioyales2@gmail.com>
2019-08-15 10:08:37 +0200

    the kernel is now written here

---
+++ CMSSW/CMSSW_10_6_0_Patatrack/src/HeterogeneousCore/NumberProducer/plugins/cudaWrapper.cu
@@ -0,0 +1,39 @@
+#include <cuda.h>
+#include "HeterogeneousCore/CUDAUtilities/interface/cudaCheck.h"
+#include <iostream>
+#include "header.h"
+
+
+
+
+
+
+
+
+
+
+//void AccumulatorGPU(double* sum_d)
+__global__ 
+void AccumulatorGPU(double* buf_d, double* sum_d, int len)
+{
+    sum_d[0] = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        sum_d[0] += buf_d[i];
+    }
+
+    printf("[cudaWrapper::AccumulatorGPU]: Result: %.5f\n", sum_d[0]);
+}
+
+void cudaWrapper(double* buf_d, double* sum_d, int len)
+{
+    
+    //AccumulatorGPU<<<1, 1>>>(sum_d);
+    cudaCheck( cudaDeviceSynchronize()); // wait for everyone
+    LOG("[cudaWrapper::cudaWrapper]:  calling the kernel", 1);
+    AccumulatorGPU<<<1, 1>>>(buf_d, sum_d, len);
+    LOG("[cudaWrapper::cudaWrapper]:  kernel called", 1);
+    cudaCheck( cudaDeviceSynchronize() ); // wait for everyone
+    LOG("[cudaWrapper::cudaWrapper]:  kernel finished", 1);
+}
