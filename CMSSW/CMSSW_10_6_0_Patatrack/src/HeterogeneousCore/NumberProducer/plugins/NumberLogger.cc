e1d2bd473d3c26879c08dab373eb883da66aad97
Mario <marioyales2@gmail.com>
2019-08-15 10:05:25 +0200

    added logs

--- CMSSW/CMSSW_10_6_0_Patatrack/src/HeterogeneousCore/NumberProducer/plugins/NumberLogger.cc
+++ CMSSW/CMSSW_10_6_0_Patatrack/src/HeterogeneousCore/NumberProducer/plugins/NumberLogger.cc
@@ -35,18 +35,21 @@ void LOG(std::string message, int t)
     /*
     t  ==  0 -> info  (white)
     t  ==  1 -> debug (blue)
+    t  ==  2 -> info2 (cyan)
     t  == -1 -> error (red)
     */
 
     bool info  = 1;
     bool debug = 1;
     bool error = 1;
+    bool info2 = 1;
 
     switch (t)
     {
         case -1: if (error) {std::cout << "\033[1;31m" << message << "\033[0m" << std::endl;} ; break;
         case  0: if (info)  {std::cout << "\033[0;32m" << message << "\033[0m" << std::endl;} ; break;
         case  1: if (debug) {std::cout << "\033[1;34m" << message << "\033[0m" << std::endl;} ; break;
+        case  2: if (info2) {std::cout << "\033[1;96m" << message << "\033[0m" << std::endl;} ; break;
 
         default:
             break;
@@ -57,7 +60,7 @@ void LOG(std::string message, int t)
 NumberLogger::NumberLogger(const edm::ParameterSet& config) :
     data_(consumes<std::vector<double>>(config.getParameter<edm::InputTag>("data")))
 {
-    LOG("[NumberLogger::NumberLogger]:  Constructor called", 0);
+    LOG("[NumberLogger::NumberLogger]:  Constructor called", 2);
     // now do what ever other initialization is needed
 }
 
@@ -70,7 +73,8 @@ NumberLogger::analyze(edm::StreamID sid, const edm::Event& event, const edm::Eve
     auto const& data = * handle;
 
     for (double item: data)
-        std::cout << item << " ";
+        LOG(std::to_string(item), 2);
+        //std::cout << item << " ";
     printf("\n");
 }
 
