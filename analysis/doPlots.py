478230d6a431d1bb0591cf89fea01b50b313eba3
Mario <marioyales2@gmail.com>
2019-08-19 15:21:14 +0200

    now it is more universal.

--- analysis/doPlots.py
+++ analysis/doPlots.py
@@ -1,65 +1,19 @@
 import numpy as np
 
 from plotter import Plotter
+from plotter import meanAndStd
 
-def meanAndStd2(path, fileName, num_files):
 
-    mean = []
-    error = []
 
-    # This will contain all the data.
-    matrix = np.array([None]*num_files)   # matrix[len(data)][0...num_files]
+def getFilePaths(fileName, numFiles = 10):
 
-    for i in range (num_files):
+    filePaths = []
 
-        if ucxtHH: filePath = "./data/fullRange/" + str(i) + "/UCX_transports/" + file
-        else: filePath = "./data/fullRange/" + str(i) + "/" + fileName
-
-        # We only load it $num_file times. The prize is the memory.
-        data = np.loadtxt(filePath)
-
-        #tempColumn = []
-        for j in range(len(data)):
-            #tempColumn.append(data[j][1])
-            matrix[i][j] = data[j][1]
-
-        #matrix[i] = tempColumn
-
-    for i in range(len(matrix[0])):
-
-        means = [matrix[k][i]*1e6 for k in range(num_files)]
-        mean.append(np.mean(means))
-        error.append(np.std(means))
-
-    return [mean, error]
-
-def meanAndStd(file):
-
-    mean = []
-    error = []
-
-    matrix = np.array([None]*10)   # matrix[len(data)][sample(0...9)]
-
-    for i in range (10):
-
-        if ucxtHH: filePath = "./data/fullRange/" + str(i) + "/UCX_transports/" + file
-        else: filePath = "./data/fullRange/" + str(i) + "/" + file
-
-        data = np.loadtxt(filePath)
-
-        tempColumn = []
-        for j in range(len(data)):
-            tempColumn.append(data[j][1])
-
-        matrix[i] = tempColumn
-
-    for i in range(len(matrix[0])):
-
-        means = [matrix[k][i]*1e6 for k in range(10)]
-        mean.append(np.mean(means))
-        error.append(np.std(means))
-
-    return [mean, error]
+    for i in range(numFiles):
+        if ucxtHH:  filePaths.append("./data/fullRange/" + str(i) + "/UCX_transports/" + fileName)
+        else:       filePaths.append("./data/fullRange/" + str(i) + "/" + fileName)
+    
+    return filePaths
 
 
 ucxtHH = 0
@@ -100,17 +54,17 @@ else:
 ###    ob1_openIB =   meanAndStd("ob1_openIB_110.dat")[0][1:]
 ###    ob1_openIB_err = meanAndStd("ob1_openIB_110.dat")[1][1:]
 ###
-###    ob1_tcp =   meanAndStd("ob1_tcp_110.dat")[0][1:]
-###    ob1_tcp_err = meanAndStd("ob1_tcp_110.dat")[1][1:]
-###
-###    none_none =   meanAndStd("none_none_110.dat")[0][1:]
-###    none_none_err = meanAndStd("none_none_110.dat")[1][1:]
+    ob1_tcp =   meanAndStd(getFilePaths("ob1_tcp_110.dat"))[0][1:]
+    ob1_tcp_err = meanAndStd(getFilePaths("ob1_tcp_110.dat"))[1][1:]
 
-    ucx_none =   meanAndStd("ucx_none_110.dat")[0][1:]
-    ucx_none_err = meanAndStd("ucx_none_110.dat")[1][1:]
+    none_none =   meanAndStd(getFilePaths("none_none_110.dat"))[0][1:]
+    none_none_err = meanAndStd(getFilePaths("none_none_110.dat"))[1][1:]
 
-    ucx_none_101 =   meanAndStd("ucx_none_101.dat")[0][1:]
-    ucx_none_101_err = meanAndStd("ucx_none_101.dat")[1][1:]
+    ucx_none =   meanAndStd(getFilePaths("ucx_none_110.dat"))[0][1:]
+    ucx_none_err = meanAndStd(getFilePaths("ucx_none_110.dat"))[1][1:]
+
+    ucx_none_101 =   meanAndStd(getFilePaths("ucx_none_101.dat"))[0][1:]
+    ucx_none_101_err = meanAndStd(getFilePaths("ucx_none_101.dat"))[1][1:]
 
 
 
@@ -145,18 +99,19 @@ else:
     #p.addSubplot(pSize, ob1_noOpenIB, dataLabel=r"PML=ob1; BTL=^openIB: H$\rightarrow$H", dataStyle=style, width=width, size=size)
     
     
-###    p.addSubplot(pSize, ob1_tcp,      dataLabel=r"PML=ob1, BTL=tcp,    H$\rightarrow$H", color="C2", dataStyle=style, width=width, size=size)
-###    p.drawErrorBars(ob1_tcp_err, 0, 0, color="C2")
+#    p.addSubplot(pSize, ob1_tcp,      dataLabel=r"PML=ob1, BTL=tcp,    H$\rightarrow$H", color="C2", dataStyle=style, width=width, size=size)
+#    p.drawErrorBars(ob1_tcp_err, 0, 0, color="C2")
 ###    
 ###    p.addSubplot(pSize, ob1_openIB,   dataLabel=r"PML=ob1, BTL=openIB, H$\rightarrow$H", color="C9", dataStyle=style, width=width, size=size)
 ###    p.drawErrorBars(ob1_openIB_err, 0, 0, color="C9")
 ###    
-###    p.addSubplot(pSize, none_none,    dataLabel=r"PML=( ), BTL=( ),    H$\rightarrow$H", color="C5", dataStyle=style, width=width, size=size)
-###    p.drawErrorBars(none_none_err, 0, 0, color="C5")
-    
-    p.addSubplot(pSize, ucx_none,     dataLabel=r"PML=UCX, BTL=( ),    H$\rightarrow$H", color="C1", dataStyle=style,  width=width, size=size)
-    p.drawErrorBars(ucx_none_err, 0, 0, color="C1")
-    
+#    p.addSubplot(pSize, none_none,    dataLabel=r"PML=( ), BTL=( ),    H$\rightarrow$H", color="C5", dataStyle=style, width=width, size=size)
+#    p.drawErrorBars(none_none_err, 0, 0, color="C5")
+#    
+#    p.addSubplot(pSize, ucx_none,     dataLabel=r"PML=UCX, BTL=( ),    H$\rightarrow$H", color="C1", dataStyle=style,  width=width, size=size)
+#    p.drawErrorBars(ucx_none_err, 0, 0, color="C1")
+#    
+    p.kfactor = 33
     p.addSubplot(pSize, ucx_none_101, dataLabel=r"PML=UCX, BTL=( ),    H$\rightarrow$D", color="C3", dataStyle=".:", width=width, size=size)
     p.drawErrorBars(ucx_none_101_err, 0, 0, color="C3")
 
