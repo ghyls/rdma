a75d82d77712a30863ef5f0b1ccd8823cf37714b
Mario <marioyales2@gmail.com>
2019-09-06 14:04:43 +0200

    cleaned the code

---
+++ MPI_IB_tests/cpu_usage_test/fullTest/cudaWrapper.cu
@@ -0,0 +1,26 @@
+#include <cuda.h>
+#include <iostream>
+#include "header.h"
+
+
+
+//void AccumulatorGPU(double* sum_d)
+__global__ 
+void AccumulatorGPU(double* buf_d, double* sum_d, int len)
+{
+    sum_d[0] = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        sum_d[0] += buf_d[i];
+    }
+
+    //printf("[cudaWrapper::AccumulatorGPU]: Result: %.5f\n", sum_d[0]);
+}
+
+void cudaWrapper(double* buf_d, double* sum_d, int len)
+{
+    cudaDeviceSynchronize(); // wait for everyone
+    AccumulatorGPU<<<1, 1>>>(buf_d, sum_d, len);
+    cudaDeviceSynchronize(); // wait for everyone
+}
